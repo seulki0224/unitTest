37f0003618ebce186081057a05ed37e6d8b5e05f
seulki0224 <sur1988@naver.com>
2024-02-18 14:31:53 +0900

    중복함수매개변수받는것으로 변경

--- Templete/toggle/theme.js
+++ Templete/toggle/theme.js
@@ -1,150 +1,70 @@
+console.log("1. pre 목록 display: hidden");
+function preHidden () {
 let eleListPre = document.querySelectorAll('.mainBox > pre');
     eleListPre.forEach(element => {
         element.classList.add('ishidden');
     });
+}
+preHidden();
 
-console.log("1. start 클래스이름넣기----------");
-//1. .mainbox, h3subList, inbox, pre, key 등 클래스 이름 번호 붙이기
-//1.1 .mainBox > targetMB()
+console.log("2. main, h3 class 이름 추가");
 let mainBox = document.querySelectorAll('.mainBox');
-// console.log(mainBox);
-// function setIDX2(mainBoxSelect1){
-function mainAddCls (mainBox){
-    let result = [];
-    mainBox.forEach(function mbEle(mbEle, mbIDX) {
-        mainBox[mbIDX].style.display ="block";
-        mbIDX++;
-        //2. 클래스 이름 추가
-        mbEle.classList.add(`mainBox${mbIDX}`);
-        //2.1 추가된 클래스이름만 추출
-        mbEle = mbEle.classList[1];
-        // console.log("mbEle = ", mbEle);
-        //2.2 리턴값에 삽입
-        result.push(mbEle);
-        // console.log("result = ",result);
-    });
-    return result;
-}
-let MBTarget = mainAddCls(mainBox);
-
-
-//1.2 .h3subList > targetH3()
 let h3SubList = document.querySelectorAll('.h3subList');
-function h3Addcls(){
+let clssList = [mainBox, h3SubList];
+
+function addCLS(clssList) {
     let result = [];
-    h3SubList.forEach((h3Ele, h3IDX)=> {
-        h3SubList[h3IDX].style.display ="block";
-        h3IDX++;
+    let clssList_ = clssList;
+
+    clssList_.forEach(function Elements(ele, idx) {
+        clssList_[idx].style.display = "block";
+        idx++;
+        eleName =  ele.classList[0];
         //2. 클래스 이름 추가
-        h3Ele.classList.add(`h3SubList${h3IDX}`);
+        ele.classList.add(`${eleName}`+`${idx}`);
         //2.1 추가된 클래스이름만 추출
-        h3Ele = h3Ele.classList[1];
+        ele = ele.classList[1];
         //2.2 리턴값에 삽입
-        result.push(h3Ele);
+        result.push(ele);
     });
     return result;
 }
-// console.log(h3Addcls(h3SubList));
-let H3Target = h3Addcls(h3SubList);
-console.log("targetH3 : ", H3Target);
-console.log("1. end 클래스이름넣기----------");
+addCLS(mainBox);
+addCLS(h3SubList);
+
+console.log("3. start h3SubList 요소 루프 준비");
+    h3SubList.forEach((h3BTN, h3btnIDX) => {
+        h3btnIDX++;
+
+        let hoverToggle = () =>{
+
+            // h3BTN.addEventListener('mouseover', (event) => {
+            h3BTN.addEventListener('mouseover', () => {
+
+                // let targeth3Cls = event.target.className;
+                // console.log(targeth3Cls);
+
+                let targetIDX = h3btnIDX -1;
+                let targeth3text = h3SubList[targetIDX].textContent;
+                console.log('.mainBox' + h3btnIDX, '\n' + targeth3text);
 
 
+                let targetMBIDX = '.mainBox' + h3btnIDX;
 
-console.log("2. start h3SubList 요소영역으로 잡고 이벤트 실행 준비---------");
-h3SubList.forEach(function(h3BTN, h3btnIDX){
-
-    // console.log(H3Target);
-    h3btnIDX++;
-
-    
-    function clickToggle (){
-        h3BTN.addEventListener('click', function(event) {
-        console.log("3. start 클릭이벤트----------");
-                
-            let targeth3Cls = event.target.className;
-            console.log('클릭한 타겟 : ', targeth3Cls, '.mainBox'+h3btnIDX);
-
-            let targetMBIDX = '.mainBox'+h3btnIDX;
-
-                //이건 클릭 후 가져오자 
-                let eleList = document.querySelectorAll(`${targetMBIDX}`+'> :not(.h3subList)');
-                console.log("eleList : ", eleList);
-
-                console.log("4. start ele이름 및 스타일 추가--------");
-                function eleAddcls(eleList){
-                // function eleAddcls(){
-                    let result = [];
-                    eleList.forEach((eleEle, eleIDX)=> {
-                        eleIDX++;
-                        //2. 클래스 이름 추가
-                        eleEle.classList.add(`eleList${eleIDX}`);
-                        eleEle.classList.add(`eleList`);
-                        //2.1 추가된 클래스이름만 추출
-                        eleEle = eleEle.classList[1];
-                        //2.2 리턴값에 삽입
-                        result.push(eleEle);
-                        //console.log("result = ",result);
-                    });
-                    return result;
-                }
-                let eleTarget = eleAddcls(eleList);
-                console.log("eleTarget : ", eleTarget);
-                console.log("4. end ele이름 및 스타일 추가--------");
-
-                //여까지 오케이
-                console.log(targetMBIDX);
-
-                let MBset = document.querySelectorAll(targetMBIDX);
-                console.log(MBset);
-
-                // let eleList2 = document.querySelectorAll(`${targetMBIDX}`+'> :not(.h3subList)');
-                let eleList2 = document.querySelectorAll(`${targetMBIDX}`+'> pre');
-                console.log("eleList2 : ", eleList2);
-
-                eleList2.forEach(function(eleList2){
-                    console.log(eleList); 
-                    console.log(MBset); 
-                        let bbb = document.querySelector('.mainBox1 > .eleList');
-
-                        eleList2.classList.toggle('ishidden');
-                            console.log("if eleList : ", eleList2);
-
-                });
-
-
-            console.log("3. end 클릭이벤트----------");
-            return;
-        })
-    };
-    // clickToggle();
-
-    function hoverToggle (){
-
-    // h3BTN.addEventListener('click', function(event) {
-        console.log("3. start 클릭이벤트----------");
-
-        h3BTN.addEventListener('mouseover', function(event) {
-        console.log("3. start mouseover이벤트----------");
-                
-            let targeth3Cls = event.target.className;
-            console.log('클릭한 타겟 : ', targeth3Cls, '.mainBox'+h3btnIDX);
-    
-            let targetMBIDX = '.mainBox'+h3btnIDX;
-    
                 let MBColor = document.querySelector(`${targetMBIDX}`);
-                console.log("h3Color : ", MBColor);
+                // console.log("h3Color : ", MBColor);
                 MBColor.style.border = "1px solid rgba(128, 128, 128, 0.5)";
 
                 //이건 클릭 후 가져오자 
-                let eleList = document.querySelectorAll(`${targetMBIDX}`+'> :not(.h3subList)');
-                console.log("eleList : ", eleList);
-    
-                console.log("4. start ele이름 및 스타일 추가--------");
-                function eleAddcls(eleList){
-                // function eleAddcls(){
+                let eleList = document.querySelectorAll(`${targetMBIDX}` + '> :not(.h3subList)');
+                // console.log("eleList : ", eleList);
+
+                // console.log("4. start ele이름 및 스타일 추가--------");
+
+                function eleAddcls(eleList) {
+                    // function eleAddcls(){
                     let result = [];
-                    eleList.forEach((eleEle, eleIDX)=> {
+                    eleList.forEach((eleEle, eleIDX) => {
                         eleIDX++;
                         //2. 클래스 이름 추가
                         eleEle.classList.remove(`eleList${eleIDX}`);
@@ -158,95 +78,85 @@ h3SubList.forEach(function(h3BTN, h3btnIDX){
                     return result;
                 }
                 let eleTarget = eleAddcls(eleList);
-                console.log("eleTarget : ", eleTarget);
-                console.log("4. end ele이름 및 스타일 추가--------");
-    
-                //여까지 오케이
-                console.log(targetMBIDX);
-    
+                // console.log("eleTarget : ", eleTarget);
+
+                // console.log(targetMBIDX);
+
                 let MBset = document.querySelectorAll(targetMBIDX);
-                console.log(MBset);
-    
-                // let eleList2 = document.querySelectorAll(`${targetMBIDX}`+'> :not(.h3subList)');
-                let eleList2 = document.querySelectorAll(`${targetMBIDX}`+'> pre');
-                console.log("eleList2 : ", eleList2);
-        
-                eleList2.forEach(function(eleList2){
-                    console.log(eleList); 
-                    console.log(MBset); 
-                        let bbb = document.querySelector('.mainBox1 > .eleList');
-    
-                        eleList2.classList.remove('ishidden');
-                            console.log("if eleList : ", eleList2);
-    
-                });   
-            console.log("3. end mouseover이벤트----------");
-            return;
-        })
+                // console.log(MBset);
 
-        h3BTN.addEventListener('mouseout', function(event) {
-            console.log("3. start mouseout이벤트----------");
-                    
-                let targeth3Cls = event.target.className;
-                console.log('클릭한 타겟 : ', targeth3Cls, '.mainBox'+h3btnIDX);
-        
-                let targetMBIDX = '.mainBox'+h3btnIDX;
+                let eleList2 = document.querySelectorAll(`${targetMBIDX}` + '> pre');
+                // console.log("eleList2 : ", eleList2);
 
-                let MBColor = document.querySelector(`${targetMBIDX}`);
-                console.log("h3Color : ", MBColor);
-                MBColor.style.border = "1px solid rgba(128, 128, 128, 0.1)";
-        
-                    //이건 클릭 후 가져오자 
-                    let eleList = document.querySelectorAll(`${targetMBIDX}`+'> :not(.h3subList)');
-                    console.log("eleList : ", eleList);
-        
-                    console.log("4. start ele이름 및 스타일 추가--------");
-                    function eleAddcls(eleList){
-                    // function eleAddcls(){
-                        let result = [];
-                        eleList.forEach((eleEle, eleIDX)=> {
-                            eleIDX++;
-                            //2. 클래스 이름 추가
-                            eleEle.classList.add(`eleList${eleIDX}`);
-                            eleEle.classList.add(`eleList`);
-                            //2.1 추가된 클래스이름만 추출
-                            eleEle = eleEle.classList[1];
-                            //2.2 리턴값에 삽입
-                            result.push(eleEle);
-                            //console.log("result = ",result);
-                        });
-                        return result;
-                    }
-                    let eleTarget = eleAddcls(eleList);
-                    console.log("eleTarget : ", eleTarget);
-                    console.log("4. end ele이름 및 스타일 추가--------");
-        
-                    //여까지 오케이
-                    console.log(targetMBIDX);
-        
-                    let MBset = document.querySelectorAll(targetMBIDX);
-                    console.log(MBset);
-        
-                    // let eleList2 = document.querySelectorAll(`${targetMBIDX}`+'> :not(.h3subList)');
-                    let eleList2 = document.querySelectorAll(`${targetMBIDX}`+'> pre');
-                    console.log("eleList2 : ", eleList2);
-            
-                    eleList2.forEach(function(eleList2){
-                        console.log(eleList); 
-                        console.log(MBset); 
-                            let bbb = document.querySelector('.mainBox1 > .eleList');
-        
-                            eleList2.classList.add('ishidden');
-                                console.log("if eleList : ", eleList2);
-        
-                    });   
-                console.log("3. end mouseout이벤트----------");
+                eleList2.forEach(function (eleList2) {
+                    // console.log(MBset);
+                    let bbb = document.querySelector('.mainBox1 > .eleList');
+
+                    eleList2.classList.remove('ishidden');
+                    // console.log("if eleList : ", eleList2);
+
+                });
                 return;
             })
 
-    };
-    hoverToggle();
-});
+            h3BTN.addEventListener('mouseout', function () {
+                // console.log("3. start mouseout이벤트----------");
 
-console.log("2. end  h3SubList 요소영역으로 잡고 이벤트 실행 준비----------");
+                // let targeth3Cls = event.target.className;
+                // console.log('클릭한 타겟 : ', targeth3Cls, '.mainBox' + h3btnIDX);
 
+                let targetMBIDX = '.mainBox' + h3btnIDX;
+
+                let MBColor = document.querySelector(`${targetMBIDX}`);
+                // console.log("h3Color : ", MBColor);
+                MBColor.style.border = "1px solid rgba(128, 128, 128, 0.1)";
+
+                //이건 클릭 후 가져오자 
+                let eleList = document.querySelectorAll(`${targetMBIDX}` + '> :not(.h3subList)');
+                // console.log("eleList : ", eleList);
+
+                // console.log("4. start ele이름 및 스타일 추가--------");
+
+                function eleAddcls(eleList) {
+                    // function eleAddcls(){
+                    let result = [];
+                    eleList.forEach((eleEle, eleIDX) => {
+                        eleIDX++;
+                        //2. 클래스 이름 추가
+                        eleEle.classList.add(`eleList${eleIDX}`);
+                        eleEle.classList.add(`eleList`);
+                        //2.1 추가된 클래스이름만 추출
+                        eleEle = eleEle.classList[1];
+                        //2.2 리턴값에 삽입
+                        result.push(eleEle);
+                        //console.log("result = ",result);
+                    });
+                    return result;
+                }
+                let eleTarget = eleAddcls(eleList);
+                // console.log("eleTarget : ", eleTarget);
+
+                // console.log(targetMBIDX);
+
+                let MBset = document.querySelectorAll(targetMBIDX);
+                // console.log(MBset);
+
+                // let eleList2 = document.querySelectorAll(`${targetMBIDX}`+'> :not(.h3subList)');
+                let eleList2 = document.querySelectorAll(`${targetMBIDX}` + '> pre');
+                // console.log("eleList2 : ", eleList2);
+
+                eleList2.forEach(function (eleList2) {
+                    // console.log(eleList);
+                    // console.log(MBset);
+                    let bbb = document.querySelector('.mainBox1 > .eleList');
+
+                    eleList2.classList.add('ishidden');
+                    // console.log("if eleList : ", eleList2);
+
+                });
+                return;
+            })
+
+        };
+        hoverToggle();
+    });
